13ccc06fbab17e7c60122e452823c9a35104338b
Naandalist <listiananda.apriliawan@gmail.com>
2019-10-24 09:54:59 +0700

    add javacsript

---
+++ script.js
@@ -0,0 +1,22 @@
+let button = document.querySelector(".container-button");
+let screen = document.querySelector("#screen");
+
+button.addEventListener("click", function(event){
+	//alert('its work!')
+
+	let buttonClick = event.target;
+	let buttonValue = buttonClick.innerText;
+	
+	if (buttonValue === "C"){
+		screen.value = ""
+	}
+	else if (buttonValue === "del"){
+		screen.value = screen.value.slice(0,-1)
+	} 
+	else if (buttonValue === "="){
+		screen.value = eval(screen.value)
+	} 
+	else {
+		screen.value = screen.value + buttonValue;
+	}
+});
