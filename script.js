2cd394d5ac31183392b649f4f3ef74bffc277e54
Naandalist <listiananda.apriliawan@gmail.com>
2019-10-25 22:10:35 +0700

    fixing bug

--- script.js
+++ script.js
@@ -6,16 +6,20 @@ button.addEventListener("click", function(event){
 
 	let buttonClick = event.target;
 	let buttonValue = buttonClick.innerText;
-	
+
+  if (buttonValue.length>=20){
+    screen.Value=""
+  }
+
 	if (buttonValue === "C"){
 		screen.value = ""
 	}
 	else if (buttonValue === "del"){
 		screen.value = screen.value.slice(0,-1)
-	} 
+	}
 	else if (buttonValue === "="){
 		screen.value = eval(screen.value)
-	} 
+	}
 	else {
 		screen.value = screen.value + buttonValue;
 	}
